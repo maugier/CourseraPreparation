edafb61009e38b46b32e6846dc109e6012f74583
marwan-abdellah <abdellah.marwan@gmail.com>
2013-09-01 18:39:48 +0200

    Updating the stylechecker for Java to follow the convention of the old graders.

--- JavaStyleChecking/staticCheker-Java-pmd.py
+++ JavaStyleChecking/staticCheker-Java-pmd.py
@@ -32,6 +32,11 @@ from random import Random
 from decimal import Decimal
 import random
 
+# PMD source on the sever 
+# This variable has to be changed after deploying the PMD package on the server
+# Like /opt/PMD
+PMD_SOURCE = "/home/abdellah/Desktop/Coursera/CourseraPreparation/JavaStyleChecking/pmd/"
+
 """
 NOTES: 
 * The static checker shoud be running on the .java files after being compiled.
@@ -81,80 +86,47 @@ NOTES:
 """
 
 """
-! Runs the static checker and dumps the report file to be parsed 
-for grading. 
-"""
-def runStaticChecker(srcFile, reportFilePath, pmdScript, pmdRule):
-
-	# Command for running the static checker 
-	shellCommand = pmdScript 
-	
-	# Indicate the checker 
-	shellCommand += " pmd -d "
-	
-	# File to be checked 
-	shellCommand += srcFile
-	 
-	# For command line user interface 
-	shellCommand += " -f text" 
-	
-	# Rule set 
-	shellCommand += " -R " + pmdRule
-	
-	# Version 
-	shellCommand += " -version 1.7"
-	
-	# Language 
-	shellCommand += " -language java"
-	
-	# Output report file that contains all the warnings messages 
-	shellCommand += ">>" + reportFilePath 
-	
-	print "Bash Command : \t " + shellCommand + "\n"
-	subprocess.call(shellCommand, shell=True)
-
-"""
 ! Parses the report file and return the total number of lost points 
   and the messages  
 """
-def parseReportFile(reportFilePath):
+def parse_report_file(report_file_path):
 	
 	# Report data in details 
-	reportData = ""
+	report_data = ""
 		
 	# Open the report file 
-	reportFile = open(reportFilePath, 'r')
+	report_file = open(report_file_path, 'r')
 	
 	# Initial number of warning 
-	numWarn = 0
+	num_warnings = 0
 
 	# Read every single line in the file and parse it. 
-	for iLine in reportFile:
+	for i_line in report_file:
 		
 		# Check for the logged warning messages.  
-		if (":" in iLine):
-			record = iLine.split(":")
+		if (":" in i_line):
+			record = i_line.split(":")
 			
 			# The record always has 3 parts, else, there is some problem.
 			if (len(record) == 3):
 				
 				# If the record contains always 2 parts, then that's a warning 
 				# report it and increment the number of warnings.
-				numWarn = numWarn + 1
-				reportData += "@Line:" + str(record[1]) + ": " + record[2] + "\n"
-	return (reportData, numWarn)
+				num_warnings = num_warnings + 1
+				report_data += "@Line:" + str(record[1]) + ": " + record[2] + "\n"
+	return (report_data, num_warnings)
 		
 """
 ! Returns the total number of lost points from the static checker 
 """
-def getLostPoints(numWarnings):
+def get_lost_points(num_warnings):
 	
 	# Penalty for a single warning 
-	warningPenalty = -1
+	warning_penalty = -1
 	
 	# Total lost points 
-	lostPoints = numWarnings * warningPenalty 
-	return lostPoints 
+	lost_points = num_warnings * warning_penalty 
+	return lost_points 
 
 """
 # TODOs 
@@ -162,29 +134,29 @@ def getLostPoints(numWarnings):
 # Define the path to the PMD "run.sh" script (normal under the bin directory).
 # Define the path to the checking rules that the grader will be consider. 
 """
-def main():
+def run_java_static_checker(src_file):
 	# Java source file to be checked "will be passed to the main function. 
 	# If more than one file needs to be checked, then you just pass the parent directory
 	# that contains all the files.  
-	srcFile = "/home/abdellah/Desktop/Coursera/CourseraPreparation/JavaStyleChecking/code/src/sample.java" 
+	src_file = "/home/abdellah/Desktop/Coursera/CourseraPreparation/JavaStyleChecking/code/src/sample.java" 
 
 	# The report file that contains all the warning messages. 
 	# For the moment, I just put it in the curent directory, ans I append to it 
 	# the new messages every time I just run a new rule check. 
 	# We need to clean the file at the first run sp that we can append the 
 	# different messages from the different rules. 
-	reportFilePath = os.getcwd() + "/styleChecker.report"
-	subprocess.call("rm " + reportFilePath, shell=True)
+	report_file_path = os.getcwd() + "/styleChecker.report"
+	subprocess.call("rm " + report_file_path, shell=True)
 	
 	"""
 	PMD stuff, should be passed to the @main function.  
 	"""
-	# PMD script  
-	pmdScript = "/home/abdellah/Desktop/Coursera/CourseraPreparation/JavaStyleChecking/pmd/pmd-src-5.0.4/bin/run.sh"
-	# pmdScript = pmdDir + "/pmd-src-5.0.4/bin/run.sh"
+	# PMD script, and rule path   
+	pmd_Script = PMD_SOURCE + "/pmd-src-5.0.4/bin/run.sh"
+	pmd_rule_path = PMD_SOURCE + "/pmd-bin-5.0.4/rulesets/java/"
 	
 	# PMD rule 
-	pmdRuleList =  ["android.xml", 
+	pmd_rule_list =  ["android.xml", 
 					"basic.xml",
 					"braces.xml",
 					"clone.xml",
@@ -215,34 +187,50 @@ def main():
 					"unnecessary.xml",
 					"unusedcode.xml"]
 	
-	
-	pmdRulePath = "/home/abdellah/Desktop/Coursera/CourseraPreparation/JavaStyleChecking/pmd/pmd-bin-5.0.4/rulesets/java/"
-	#pmdRulePath = pmdDir + "//pmd-bin-5.0.4/rulesets/java/"
-	
 	# Check the selected rules in the list "CUSTOMIZABLE"  
-	for rule in pmdRuleList:
-		pmdRule = pmdRulePath + rule 
+	for rule in pmd_rule_list:
+		pmd_rule = pmd_rule_path + rule 
 		
-		# Run the static checker with the specified rule and dump the result to the report file  
-		runStaticChecker(srcFile, reportFilePath, pmdScript, pmdRule)
+		# Command for running the static checker 
+		shell_command = pmd_Script 
+	
+		# Indicate the checker 
+		shell_command += " pmd -d "
+	
+		# File to be checked 
+		shell_command += src_file
+		 
+		# For command line user interface 
+		shell_command += " -f text" 
+	
+		# Rule set 
+		shell_command += " -R " + pmd_rule
+	
+		# Version 
+		shell_command += " -version 1.7"
+	
+		# Language 
+		shell_command += " -language java"
+	
+		# Output report file that contains all the warnings messages 
+		shell_command += ">>" + report_file_path 
+	
+		print "Bash Command : \t " + shell_command + "\n"
+		subprocess.call(shell_command, shell=True)
 	
 	# Parse the generated report file.  
-	warningData, numWarnings = parseReportFile(reportFilePath)
+	warning_data, num_warnings = parse_report_file(report_file_path)
 	
 	# Display the lost points and the final message that will be sent 
 	# back to the student indicating the total number of lost marks. 
-	# This message is just for testing purpose 
-	print("Total number of lost points is " + str(getLostPoints(numWarnings)))
+	# This message is just for testing purpose, but it can be printed to the 
+	# log file  
+	print("Total number of lost points is " + str(get_lost_points(num_warnings)))
 	print("The final messges will be : " )
-	print (warningData)
+	print (warning_data)
+	
 
 
-
-# Run the main function. 
-# You have to pass to the main function where we keep the bin and the src
-# directories of PMD. You will find the version used to develop this grader 
-# uploaded to the course repository. 
-main()
 	
 	
 	
